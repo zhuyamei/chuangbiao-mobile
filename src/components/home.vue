e37b93653acdb94a76527e24ad208f40a2e73192
zhuyamei <2388028392@qq.com>
2020-03-13 17:09:16 +0800

    guanwang logs

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    官网首页
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
